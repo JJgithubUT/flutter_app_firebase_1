a35884f7df8f74f4e82a4c0aa7ad7297c4bbc9b9
JuanJavier <juanjcbreton@gmail.com>
2025-03-14 12:01:47 -0600

    14-3-25 CRUD funcional para artista

--- lib/screens/artist_screen.dart
+++ lib/screens/artist_screen.dart
@@ -16,38 +16,80 @@ class _ArtistScreenState extends State<ArtistScreen> {
   final TextEditingController _inicioController = TextEditingController();
   final TextEditingController _albumsController = TextEditingController();
 
-  void insertArtist() {
-    _cloudFirestoreService.insertArtist('artistas', 
-      {
-        'nombre': _nombreController.text,
-        'genero': _generoController.text,
-        'albums': int.parse(_albumsController.text),
-        'inicio': int.parse(_inicioController.text),
-      }      
-    );
+  void clearFields() {
     _nombreController.clear();
     _generoController.clear();
     _albumsController.clear();
     _inicioController.clear();
   }
 
+  void insertArtist() {
+    _cloudFirestoreService.insertArtist('artistas', {
+      'nombre': _nombreController.text,
+      'genero': _generoController.text,
+      'albums': int.parse(_albumsController.text),
+      'inicio': int.parse(_inicioController.text),
+    });
+    clearFields();
+  }
+
   void updateArtist(String docId) {
-    _cloudFirestoreService.updateArtist('artistas', docId,
-      {
-        'nombre': _nombreController.text,
-        'genero': _generoController.text,
-        'albums': int.parse(_albumsController.text),
-        'inicio': int.parse(_inicioController.text),
-      }
-    );
-    _nombreController.clear();
-    _generoController.clear();
-    _albumsController.clear();
-    _inicioController.clear();
+    _cloudFirestoreService.updateArtist('artistas', docId, {
+      'nombre': _nombreController.text,
+      'genero': _generoController.text,
+      'albums': int.parse(_albumsController.text),
+      'inicio': int.parse(_inicioController.text),
+    });
+    clearFields();
   }
 
   void deleteArtist(String docId) {
     _cloudFirestoreService.deleteArtist('artistas', docId);
+  }
+
+  void showDialogInsert() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Añadir artista'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _nombreController,
+                decoration: const InputDecoration(labelText: 'Nombre'),
+              ),
+              TextField(
+                controller: _generoController,
+                decoration: const InputDecoration(labelText: 'Género'),
+              ),
+              TextField(
+                controller: _albumsController,
+                decoration: const InputDecoration(labelText: 'Albums'),
+              ),
+              TextField(
+                controller: _inicioController,
+                decoration: const InputDecoration(labelText: 'Inicio'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cancelar'),
+            ),
+            TextButton(
+              onPressed: () {
+                insertArtist();
+                Navigator.of(context).pop();
+              },
+              child: const Text('Agregar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void showDialogEdit(ArtistModel artist) {
@@ -93,23 +135,32 @@ class _ArtistScreenState extends State<ArtistScreen> {
                 updateArtist(artist.id);
                 Navigator.of(context).pop();
               },
-              child: const Text('Actualizar')
-            )
+              child: const Text('Actualizar'),
+            ),
           ],
         );
-      }
-      );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Mis artistas de Spotify'),
-      ),
+          title: Text('Mi App'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                showDialogInsert();
+              },
+            ),
+          ],
+        ),
       body: Column(
         children: [
-          TextField(
+
+          /* TextField(
             controller: _nombreController,
             decoration: const InputDecoration(labelText: 'Nombre'),
           ),
@@ -128,7 +179,9 @@ class _ArtistScreenState extends State<ArtistScreen> {
           ElevatedButton(
             onPressed: insertArtist,
             child: const Text('Agregar artista'),
-          ),
+          ), */
+
+          const SizedBox(height: 20),
           Expanded(
             child: StreamBuilder(
               stream: _cloudFirestoreService.getArtists('artistas'),
@@ -140,17 +193,18 @@ class _ArtistScreenState extends State<ArtistScreen> {
                   return const CircularProgressIndicator();
                 }
                 return ListView(
-                  children: snapshot.data!.map((ArtistModel artist) {
-                    return ListTile(
-                      title: Text(artist.name),
-                      subtitle: Text('Genero: ${artist.genre}'),
-                      onTap: () => showDialogEdit(artist),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () => deleteArtist(artist.id),
-                      ),
-                    );
-                  }).toList(),
+                  children:
+                      snapshot.data!.map((ArtistModel artist) {
+                        return ListTile(
+                          title: Text(artist.name),
+                          subtitle: Text('Genero: ${artist.genre}'),
+                          onTap: () => showDialogEdit(artist),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () => deleteArtist(artist.id),
+                          ),
+                        );
+                      }).toList(),
                 );
               },
             ),
